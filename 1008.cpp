032ae8be68824f3413f373bc7973071aac16435d
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-06-21 06:53:00 +0530

    Added solution

--- 1008.cpp
+++ 1008.cpp
@@ -11,43 +11,25 @@ bool is_consonant(char c) {
 
 
 int main() {
-
   string s;
-
   cin >> s;
-
   char last = s[s.size() - 1];
-
   if (is_consonant(last) and last != 'n') {
-
     cout << "NO" << "\n";
-
     return 0;
-
   }
-
   bool ok = true;
-
   for (int i = 0; ok and i < s.size() - 1; i++) {
-
     if (s[i] != 'n' and is_consonant(s[i]) and is_consonant(s[i + 1])) {
-
       ok = false;
-
       break;
-
     }
-
   }
 
   if (ok) {
-
     cout << "YES" << "\n";
-
   } else {
-
     cout << "NO" << "\n";
-
   }
 
   return 0;
