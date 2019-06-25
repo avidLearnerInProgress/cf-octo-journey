ade24d54933cb684d868f250ecc931924339d4ec
avidLearnerInProgress <chiragshah9696@gmail.com>
2019-06-25 16:53:20 +0530

    Added solution

--- 501b.cpp
+++ 501b.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h> // Include every standard library 
 using namespace std; 
-
 typedef long long LL; 
 typedef pair<int, int> pii; 
 typedef pair<LL, LL> pll; 
@@ -10,7 +9,6 @@ typedef vector<vi> vvi;
 typedef vector<pii> vii; 
 typedef vector<LL> vl; 
 typedef vector<vl> vvl; 
-
 double EPS=1e-9; 
 int INF=1000000005; 
 long long INFF=1000000000000000005LL; 
